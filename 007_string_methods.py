9f3037e82c5887b30c30799d251f82b684db54d7
vanyabooo <rise.vane@gmail.com>
2024-02-06 15:27:02 +0300

    update 000-010

---
+++ 007_string_methods.py
@@ -0,0 +1,8 @@
+course = 'Python for beginners'
+# print(len(course)) // кол-во символов len
+# # Методы - upper/lower - выводят string в требуемом регистре, но не изменяют его
+# print(course.upper())
+# print(course.replace('beginners', 'Habibi'))
+# print(course.replace('n', 'V'))
+print('Python' in course)
+# print(course.title()) // первая буква заглавная в каждом слове
