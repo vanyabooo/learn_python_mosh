b00274cb41dffffc7145168448fc577b650a9d43
vanyabooo <rise.vane@gmail.com>
2024-02-12 10:29:50 +0300

    update 000-012

--- 009_arithmetic_operations.py
+++ 009_arithmetic_operations.py
@@ -1,4 +1,25 @@
 x = 10
-x = x + x + x + 3
-x = 3
-print(x)
+y = 10
+
+
+# Augmented assignment operator is a shorter way to reassign a variable using itself
+x+= 2
+print(f"Using augmented assignment operator: {x}")
+
+y = y +2
+print(f"NOT using augmented assignment operator: {y}")
+
+# Other notable arithmetic operators
+print()
+print(f"Regular division would be 'a = 20 / 6' where a = {20 / 6}")
+
+print()
+print(f"Division without a remainder 'b = 20 / 6' where b = {20 // 6}")
+
+print()
+print(f"Division, but just the remainder 'c = 20 % 6' where c = {20 % 6}")
+print(f"This is read as 'mod' or 'modulus'.. As in '20 mod 6'.")
+
+print()
+print(f"Exponent or Power 'k = 4 ** 2' where k = {4 ** 2}")
+
---
+++ 011_math_functions.py
@@ -0,0 +1,25 @@
+import math as m
+from math import *
+from math import ceil, cos, sin
+from math import sin as s
+from learn_python_mosh import README
+
+'''
+Можно вызвать библиотеку, через as обозвать ее как угодно
+Можно выбрать папку/библиотеку, и из нее файл
+ceil метод округляет до ближайшего большего числа
+floor метод округляет до ближайшего нижнего числа
+'''
+
+print(m.ceil(2.1))
+
+print(ceil(x))
+
+
+
+# xuy = 2.8
+# round функция округляет значение до целого
+# print(round(xuy))
+# abs функция делает любое значение положительным
+# print(abs(-2.9))
+
